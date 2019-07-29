bb1f173a3ab80cb61a7fdab0d7fd69c25ec9be9a
diegovivas <dieguitovivas@hotmail.com>
2019-07-29 17:04:42 +0000

    Feature #7: function print unsigned int to hexadecimal

---
+++ print_hexalower.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * print_hexalow - convert unsigned int in hexadecimal
+ *@n: unsigned int
+ * Return: numer times print
+ */
+int print_hexalow(unsigned int n)
+{
+	long int decimalNumber, remainder, quotient;
+	int cont = 1, cont2, temp;
+	char hexadecimalNumber[100];
+	int count = 0;
+
+	quotient = n;
+	while (quotient != 0)
+	{
+		temp = quotient % 16;
+
+		if (temp < 10)
+			temp = temp + 48;
+		else
+			temp = temp + 87;
+
+		hexadecimalNumber[cont++] = temp;
+		quotient = quotient / 16;
+	}
+	for (cont2 = cont1 - 1 ; cont2 > 0; cont2--, count++)
+		_putchar(hexadecimalNumber[cont2]);
+
+	return (count);
+}
